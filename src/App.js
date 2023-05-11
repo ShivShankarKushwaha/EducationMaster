d39087192a044e3550b347f58a7c4a0851184210
ShivshankarKushwaha <shivshankarkushwaha0000@gmail.com>
2023-05-11 12:26:45 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-// import Navbar from './compt' from 'react'
+// import Navbar from './compt from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import CoursesFor10th from './components/CoursesFor10th'
 import CousesForOthersInBoards from './components/CousesForOthersInBoards'
 import Chapters from './components/Chapters'
 import CoursesForJEEAndNeet from './components/CoursesForJEEAndNeet'
-import YoutubePlayer from './components/YoutubePlayer'
+import Yt from './components/Yt'
+import { Pdf } from './components/Pdf'
+import YoutubeAndFile from './components/YoutubeAndFile'
+import Testimonial from './components/Testimonial'
 
 function App() {
   return (
@@ -26,29 +29,12 @@ function App() {
             element={<CoursesForJEEAndNeet />}
           />
           <Route exact path="/chapters" element={<Chapters />} />
-          <Route exact path="/youtube" element={<YoutubePlayer />} />
+          <Route exact path="/youtube" element={<YoutubeAndFile />} />
+          <Route exact path="/pdf" element={<Testimonial />} />
         </Routes>
       </Router>
     </>
   )
 }
-// import Home from './components/hero/Home'
-// import OurResults from './components/OurResults'
-// import OfflineCenter from './components/OfflineCenter'
-// import Table from './components/Table'
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navbar />
-//       <Home />
-//       <AndroidApp />
-//       <OurResults />
-//       <OfflineCenter />
-//       <Table />
-//     </div>
-
-//   )
-// }
 
 export default App
